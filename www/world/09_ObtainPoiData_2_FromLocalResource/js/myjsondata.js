a11c52a367a57f7d24a6c2176be939a2c689cfda
sohan1 <shn619@gmail.com>
2018-02-23 17:16:31 +0530

    first commit

---
+++ www/world/09_ObtainPoiData_2_FromLocalResource/js/myjsondata.js
@@ -0,0 +1,55 @@
+var myJsonData = [{
+	"id": "1",
+	"longitude": "-0.080179",
+	"latitude": "51.510788",
+	"description": "assets/spa.jpg",
+	"label": "Spa",
+	"name": "Pure​ & Beauty",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}, {
+	"id": "2",
+	"longitude": "-0.092502",
+	"latitude": "51.507365",
+	"description": "assets/restaurant.jpg",
+	"label": "Restaurant",
+	"name": "Nando's",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}, {
+	"id": "3",
+	"longitude": "-0.095379",
+	"latitude": "51.507110",
+	"description": "assets/playhouse.jpg",
+	"label": "Playhouse",
+	"name": "Rose",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}, {
+	"id": "4",
+	"longitude": "-0.088285",
+	"latitude": "51.504425",
+	"description": "assets/movies.jpg",
+	"label": "Movies",
+	"name": "Medicinema",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}, {
+	"id": "5",
+	"longitude": "-0.095189",
+	"latitude": "51.514131",
+	"description": "assets/shopping.jpg",
+	"label": "Shopping",
+	"name": "BOSS Store",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}, {
+	"id": "6",
+	"longitude": "-0.075346",
+	"latitude": "51.505493",
+	"description": "assets/bridge.jpg",
+	"label": "Bridge",
+	"name": "Tower Bridge",
+	"image": "assets/marker_selected.png",
+	"image2": "assets/marker_selected.png"
+}];
